720a8b2ff032faf7fb451032a19c972993a8d4ce
Daniel SalasAlonso <dsa069@inlumine.ual.es>
2024-02-21 13:43:02 +0100

    salasbranch test

--- ruizsalas/src/main/java/proyectoMDS/MainView.java
+++ ruizsalas/src/main/java/proyectoMDS/MainView.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 /**
  * A sample Vaadin view class.
  * <p>
+ * 
  * To implement a Vaadin view just extend any Vaadin component and
  * use @Route annotation to announce it in a URL as a Spring managed
  * bean.
