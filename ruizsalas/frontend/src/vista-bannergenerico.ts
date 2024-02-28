d20e4e9b252e27d1a6fa1e6cabdb0c2353b9cc66
Daniel Salas <dsa069@inlumine.ual.es>
2024-02-28 18:59:24 +0100

    mi cara cuando

--- ruizsalas/frontend/src/vista-bannergenerico.ts
+++ ruizsalas/frontend/src/vista-bannergenerico.ts
@@ -1,8 +1,8 @@
 import { LitElement, html, css, customElement } from 'lit-element';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@polymer/iron-icon/iron-icon.js';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 
 @customElement('vista-bannergenerico')
 export class VistaBannergenerico extends LitElement {
@@ -21,13 +21,27 @@ export class VistaBannergenerico extends LitElement {
  <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct); padding: var(--lumo-space-m);">
   <vaadin-vertical-layout style="flex-grow: 1; height: 100%;">
    <vaadin-button theme="icon" aria-label="Add new" style="flex-grow: 0; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
-    <iron-icon icon="lumo:plus"></iron-icon>
+    <iron-icon icon="lumo:plus" id="Icono pag principal"></iron-icon>
    </vaadin-button>
   </vaadin-vertical-layout>MOMAZOS DIEGO 
-  <vaadin-vertical-layout style="flex-grow: 1; height: 100%;">
-   <vaadin-button theme="icon" aria-label="Add new" style="flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; align-self: flex-end; padding: var(--lumo-space-m);">
-    <iron-icon icon="lumo:plus"></iron-icon>
-   </vaadin-button>
+  <vaadin-vertical-layout style="flex-grow: 0; height: 100%; flex-shrink: 1;">
+   <vaadin-horizontal-layout theme="spacing" style="flex-grow: 1; align-self: flex-end;">
+    <vaadin-button theme="icon" aria-label="Add new" id="Revisar Noticias" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+     <iron-icon icon="lumo:plus"></iron-icon>
+    </vaadin-button>
+    <vaadin-button theme="icon" aria-label="Add new" id="VerPeriodistas" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+     <iron-icon icon="lumo:plus"></iron-icon>
+    </vaadin-button>
+    <vaadin-button theme="icon" aria-label="Add new" id="MisNoticias" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+     <iron-icon icon="lumo:plus"></iron-icon>
+    </vaadin-button>
+    <vaadin-button theme="icon" aria-label="Add new" id="IniciarSesion" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+     <iron-icon icon="lumo:plus"></iron-icon>
+    </vaadin-button>
+    <vaadin-button theme="icon" aria-label="Add new" style="flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; align-self: flex-end; padding: var(--lumo-space-m);" id="Registrarse1">
+     <iron-icon icon="lumo:plus" id="Registrarse"></iron-icon>
+    </vaadin-button>
+   </vaadin-horizontal-layout>
   </vaadin-vertical-layout>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
