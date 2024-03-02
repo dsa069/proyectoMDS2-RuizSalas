b20ab1844b1e91def36a5491ecb41379938b4860
crv520-1 <crv520@inlumine.ual.es>
2024-03-02 11:51:28 +0100

    banner

--- ruizsalas/frontend/src/vista-bannergenerico.ts
+++ ruizsalas/frontend/src/vista-bannergenerico.ts
@@ -19,27 +19,27 @@ export class VistaBannergenerico extends LitElement {
   render() {
     return html`
 <vaadin-vertical-layout style="height: 100%;">
- <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct); padding: var(--lumo-space-m); height: 100px;">
-  <vaadin-horizontal-layout theme="spacing">
-   <vaadin-button theme="icon" aria-label="Add new" style="flex-grow: 0; flex-shrink: 0; height: 100%; width: 27%;">
-    <iron-icon icon="vaadin:newspaper" id="Icono pag principal" style="width: 100%; height: 100%; color: black;"></iron-icon>
+ <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct); padding: var(--lumo-space-m); height: 100px; font-family: Arial; font-size: 25px; align-items: center;">
+  <vaadin-horizontal-layout style="flex-grow: 1; flex-shrink: 0; justify-content: flex-start; width: 100px; align-self: center;">
+   <vaadin-button theme="icon" aria-label="Add new" style="flex-grow: 0; flex-shrink: 1; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+    <iron-icon icon="vaadin:newspaper" id="Icono pag principal" style="color: black; width: 100%;"></iron-icon>
    </vaadin-button>
   </vaadin-horizontal-layout>MOMAZOS DIEGO 
-  <vaadin-horizontal-layout theme="spacing" style="flex-grow: 1; flex-shrink: 0; align-self: flex-end; width: 100px;">
-   <vaadin-button theme="icon" aria-label="Add new" id="Revisar Noticias" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; padding: var(--lumo-space-m); width: 15%;">
-    <iron-icon icon="lumo:plus"></iron-icon>
+  <vaadin-horizontal-layout theme="spacing-s" style="flex-grow: 1; flex-shrink: 0; width: 100px; align-self: center; justify-content: flex-end;">
+   <vaadin-button theme="icon" aria-label="Add new" id="Revisar Noticias" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; padding: var(--lumo-space-m); width: 15%;">
+    <iron-icon icon="vaadin:edit" style="color: black; width: 100%;"></iron-icon>
    </vaadin-button>
-   <vaadin-button theme="icon" aria-label="Add new" id="VerPeriodistas" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
-    <iron-icon icon="lumo:plus"></iron-icon>
+   <vaadin-button theme="icon" aria-label="Add new" id="VerPeriodistas" style="flex-grow: 0; flex-shrink: 1; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+    <iron-icon icon="vaadin:clipboard-user" style="color: black; width: 100%;"></iron-icon>
    </vaadin-button>
-   <vaadin-button theme="icon" aria-label="Add new" id="MisNoticias" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
-    <iron-icon icon="lumo:plus"></iron-icon>
+   <vaadin-button theme="icon" aria-label="Add new" id="MisNoticias" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+    <iron-icon icon="vaadin:file-search" style="color: black; width: 100%;"></iron-icon>
    </vaadin-button>
-   <vaadin-button theme="icon" aria-label="Add new" id="IniciarSesion" style="align-self: flex-end; flex-grow: 1; flex-shrink: 0; height: 100%; width: 15%; padding: var(--lumo-space-m);">
-    <iron-icon icon="lumo:plus"></iron-icon>
+   <vaadin-button theme="icon" aria-label="Add new" id="IniciarSesion" style="flex-grow: 0; flex-shrink: 1; height: 100%; width: 15%; padding: var(--lumo-space-m);">
+    <iron-icon icon="vaadin:user" style="color: black; width: 100%;"></iron-icon>
    </vaadin-button>
-   <vaadin-button>
-    SUSCRIBIRSE
+   <vaadin-button style="align-self: center; color: black;">
+     SUSCRIBIRSE 
    </vaadin-button>
   </vaadin-horizontal-layout>
  </vaadin-horizontal-layout>
