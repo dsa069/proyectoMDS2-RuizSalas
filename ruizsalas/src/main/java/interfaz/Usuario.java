7ebdefe65b49dbfb1e852a8ab7b52d1ac013befb
Carlos Ruiz <crv520@inlumine.ual.es>
2024-03-22 12:53:43 +0100

    Me canse ya de tantas preguntas, cambie, pero no me disgusta

--- ruizsalas/src/main/java/interfaz/Usuario.java
+++ ruizsalas/src/main/java/interfaz/Usuario.java
@@ -4,14 +4,23 @@ import proyectoMDS.*;
 //import basededatos.iUsuario;
 
 public class Usuario extends VistaUsuario {
-	
+
 	public MainView mainView;
 	public Usuario(MainView MainView) {
 		this.mainView = MainView;
 	}
-//	public iUsuario _iUsuario;
-//	public Busqueda _accede;
-	
-	
+	//	public iUsuario _iUsuario;
+	//	public Busqueda _accede;
+
+	//Ns nada, pero lo subo pq es algo de lo estatico seguro
+	//	public Busqueda _busqueda;
+	//	public Usuario (MainView MainView) {
+	//		this.MainView = MainView;
+	//		_busqueda = new Buscar(this);
+	//		Busqueda();
+	//	}
+//	public void Busqueda() {
+//
+//	}
 }
 
