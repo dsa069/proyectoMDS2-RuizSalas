af1ace3eac10837ff64daa82bf50aff8648a7bb7
Daniel Salas <dsa069@inlumine.ual.es>
2024-03-27 18:23:37 +0100

    el push que acabo con nuestro proyecto

--- ruizsalas/src/main/java/interfaz/Noticia.java
+++ ruizsalas/src/main/java/interfaz/Noticia.java
@@ -1,12 +1,12 @@
 package interfaz;
 
 public class Noticia {
-//	private JScrollPanel _scroll;
-//	private JLabel _imagen_principal;
-//	private JLabel _fecha;
-//	private JLabel _autor;
-//	private JLabel _ubicacion;
-//	private JLabel _titulo;
-//	private JLabel _valoracion;
+	//private JScrollPanel _scroll;
+	//private JLabel _imagen_principal;
+	//private JLabel _fecha;
+	//private JLabel _autor;
+	//private JLabel _ubicacion;
+	//private JLabel _titulo;
+	//private JLabel _valoracion;
 	public Lista_Comentarios _contiene;
 }
--- ruizsalas/src/main/java/interfaz/Noticia_Vista_Editor.java
+++ ruizsalas/src/main/java/interfaz/Noticia_Vista_Editor.java
@@ -3,7 +3,7 @@ package interfaz;
 //import UCL Proyecto.Eliminar_noticia;
 
 public class Noticia_Vista_Editor extends Noticia_completa {
-	public Editor _unnamed_Editor_;
-	public Lista_Secciones_noticia _contiene;
+	//public Editor _unnamed_Editor_;
+	//public Lista_Secciones_noticia _contiene;
 	//public Eliminar_noticia _contiene_la_opcion_de;
 }
