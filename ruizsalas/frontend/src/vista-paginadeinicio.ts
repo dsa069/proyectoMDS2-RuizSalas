be5c20dcf646884cd13141face0eceb3d9f3ba08
crv520-1 <crv520@inlumine.ual.es>
2024-03-27 19:33:39 +0100

    Bueno menos da una piedra

--- ruizsalas/frontend/src/vista-paginadeinicio.ts
+++ ruizsalas/frontend/src/vista-paginadeinicio.ts
@@ -1,4 +1,6 @@
 import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 
 @customElement('vista-paginadeinicio')
 export class VistaPaginadeinicio extends LitElement {
@@ -12,7 +14,24 @@ export class VistaPaginadeinicio extends LitElement {
   }
 
   render() {
-    return html``;
+    return html`
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vaadin-horizontal-layout style="width: 100%; height: 20%;">
+  <vaadin-vertical-layout style="width: 100%; height: 100%;">
+   <vaadin-horizontal-layout style="width: 100%; height: 75%;"></vaadin-horizontal-layout>
+   <vaadin-horizontal-layout style="width: 100%; height: 25%;"></vaadin-horizontal-layout>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+ <vaadin-horizontal-layout style="width: 100%; height: 80%;">
+  <vaadin-vertical-layout style="width: 10%;"></vaadin-vertical-layout>
+  <vaadin-vertical-layout style="width: 80%;">
+   <vaadin-horizontal-layout style="width: 100%; height: 75%;"></vaadin-horizontal-layout>
+   <vaadin-horizontal-layout style="width: 100%; height: 25%;"></vaadin-horizontal-layout>
+  </vaadin-vertical-layout>
+  <vaadin-vertical-layout style="width: 10%;"></vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
   }
 
   // Remove this method to render the contents of this view inside Shadow DOM
