363c4a059ccb37d64a53ec439da286af58c8ed55
crv520-1 <crv520@inlumine.ual.es>
2024-03-29 13:44:59 +0100

    Pull de los get y set

--- ruizsalas/frontend/src/vista-bannersuscrito.ts
+++ ruizsalas/frontend/src/vista-bannersuscrito.ts
@@ -25,13 +25,13 @@ export class VistaBannersuscrito extends LitElement {
    </vaadin-button>
   </vaadin-horizontal-layout>MOMAZOS DIEGO 
   <vaadin-horizontal-layout style="flex-grow: 1; flex-shrink: 0; justify-content: flex-end; width: 33%; align-self: center; height: 100%;">
-   <vaadin-button theme="icon" aria-label="Add new" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; background-color: var(--lumo-contrast-0pct);">
+   <vaadin-button theme="icon" aria-label="Add new" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; background-color: var(--lumo-contrast-0pct);" id="botonRevisarNoticia">
     <img src="https://cdn-icons-png.flaticon.com/512/6848/6848160.png" width="63" height="63">
    </vaadin-button>
-   <vaadin-button theme="icon" aria-label="Add new" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; background-color: var(--lumo-contrast-0pct);">
+   <vaadin-button theme="icon" aria-label="Add new" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; background-color: var(--lumo-contrast-0pct);" id="botonIrAPeriodistas">
     <img src="https://cdn-icons-png.flaticon.com/512/1995/1995463.png" width="67" height="67">
    </vaadin-button>
-   <vaadin-button theme="icon" aria-label="Add new" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; background-color: var(--lumo-contrast-0pct);">
+   <vaadin-button theme="icon" aria-label="Add new" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; background-color: var(--lumo-contrast-0pct);" id="botonHistorialNoticia">
     <img src="https://cdn-icons-png.flaticon.com/512/9964/9964353.png" width="64" height="64">
    </vaadin-button>
    <vaadin-button theme="icon" aria-label="Add new" style="align-self: center; flex-grow: 0; flex-shrink: 0; height: 100%; background-color: var(--lumo-contrast-0pct);">
@@ -39,26 +39,26 @@ export class VistaBannersuscrito extends LitElement {
    </vaadin-button>
   </vaadin-horizontal-layout>
  </vaadin-horizontal-layout>
- <vaadin-horizontal-layout style="width: 100%; height: 10%; align-self: center; justify-content: center; font-size: 36px;">
+ <vaadin-horizontal-layout style="width: 100%; height: 10%; align-self: center; justify-content: center; font-size: 36px;" id="textoPerfil">
    Perfil 
  </vaadin-horizontal-layout>
- <vaadin-vertical-layout style="width: 100%; height: 30%; flex-shrink: 0;">
-  <vaadin-horizontal-layout style="width: 100%; flex-shrink: 0; height: 50%;"></vaadin-horizontal-layout>
-  <vaadin-horizontal-layout style="width: 100%; height: 50%; flex-shrink: 0;"></vaadin-horizontal-layout>
+ <vaadin-vertical-layout style="width: 100%; height: 30%; flex-shrink: 0;" id="noticiaLayoutGenerico">
+  <vaadin-horizontal-layout style="width: 100%; flex-shrink: 0; height: 50%;" id="noticiaLayout"></vaadin-horizontal-layout>
+  <vaadin-horizontal-layout style="width: 100%; height: 50%; flex-shrink: 0;" id="noticiaEditorLayout"></vaadin-horizontal-layout>
  </vaadin-vertical-layout>
- <vaadin-vertical-layout style="height: 25%; width: 100%; flex-shrink: 0;">
+ <vaadin-vertical-layout style="height: 25%; width: 100%; flex-shrink: 0;" id="perfilUsuarioLayout">
   <vaadin-horizontal-layout style="width: 100%; flex-shrink: 0; height: 50%;"></vaadin-horizontal-layout>
   <vaadin-horizontal-layout style="width: 100%; height: 50%; flex-shrink: 0; justify-content: space-between;">
    <vaadin-button theme="icon" aria-label="Add new" style="flex-grow: 0; flex-shrink: 1; height: 100%; background-color: var(--lumo-contrast-0pct); width: 20%;">
     <iron-icon style="width: 100%; height: 90px;" src="https://cdn-icons-png.flaticon.com/128/660/660350.png"></iron-icon>
    </vaadin-button>
-   <vaadin-horizontal-layout style="height: 100%; width: 20%;"></vaadin-horizontal-layout>
+   <vaadin-horizontal-layout style="height: 100%; width: 20%; flex-shrink: 0;" id="darseDeBajaLayout"></vaadin-horizontal-layout>
    <vaadin-button theme="icon" aria-label="Add new" style="flex-grow: 0; flex-shrink: 1; height: 100%; background-color: var(--lumo-contrast-0pct); width: 20%;">
     <iron-icon style="width: 100%; height: 90px;" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRGYVO6IvGN6qiLudolQK5Mizx4z0CAONYfOcDlZXT0u_ajOffS"></iron-icon>
    </vaadin-button>
   </vaadin-horizontal-layout>
  </vaadin-vertical-layout>
- <vaadin-vertical-layout style="width: 100%; height: 25%; flex-shrink: 0;">
+ <vaadin-vertical-layout style="width: 100%; height: 25%; flex-shrink: 0;" id="editarPerfilLayout">
   <vaadin-horizontal-layout style="width: 100%; height: 50%; flex-shrink: 0;"></vaadin-horizontal-layout>
   <vaadin-horizontal-layout style="width: 100%; height: 50%; flex-shrink: 0; justify-content: space-between; padding: var(--lumo-space-m);">
    <vaadin-button style="color: black; flex-shrink: 1; flex-grow: 0; align-self: center; font-size: 25px;">
--- ruizsalas/src/main/java/vistas/VistaNoticia.java
+++ ruizsalas/src/main/java/vistas/VistaNoticia.java
@@ -3,13 +3,81 @@ package vistas;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.button.Button;
 
 @Tag("vista-noticia")
 @JsModule("./src/vista-noticia.ts")
 public class VistaNoticia extends LitTemplate {
 
-    public VistaNoticia() {
+    @Id("tematicas")
+	private Element tematicas;
+	@Id("seleccionarTematicas")
+	private Element seleccionarTematicas;
+	@Id("SeleccionarSecciones")
+	private Element seleccionarSecciones;
+	@Id("valorarNoticia")
+	private HorizontalLayout valorarNoticia;
+	@Id("eliminarNoticia")
+	private Button eliminarNoticia;
+	@Id("noticiaCortada")
+	private Element noticiaCortada;
+	@Id("noticiaCompleta")
+	private Element noticiaCompleta;
+	@Id("botonSuscribirse")
+	private Element botonSuscribirse;
+	public VistaNoticia() {
         // You can initialise any data required for the connected UI components here.
     }
+	public Element getTematicas() {
+		return tematicas;
+	}
+	public void setTematicas(Element tematicas) {
+		this.tematicas = tematicas;
+	}
+	public Element getSeleccionarTematicas() {
+		return seleccionarTematicas;
+	}
+	public void setSeleccionarTematicas(Element seleccionarTematicas) {
+		this.seleccionarTematicas = seleccionarTematicas;
+	}
+	public Element getSeleccionarSecciones() {
+		return seleccionarSecciones;
+	}
+	public void setSeleccionarSecciones(Element seleccionarSecciones) {
+		this.seleccionarSecciones = seleccionarSecciones;
+	}
+	public HorizontalLayout getValorarNoticia() {
+		return valorarNoticia;
+	}
+	public void setValorarNoticia(HorizontalLayout valorarNoticia) {
+		this.valorarNoticia = valorarNoticia;
+	}
+	public Button getEliminarNoticia() {
+		return eliminarNoticia;
+	}
+	public void setEliminarNoticia(Button eliminarNoticia) {
+		this.eliminarNoticia = eliminarNoticia;
+	}
+	public Element getNoticiaCortada() {
+		return noticiaCortada;
+	}
+	public void setNoticiaCortada(Element noticiaCortada) {
+		this.noticiaCortada = noticiaCortada;
+	}
+	public Element getNoticiaCompleta() {
+		return noticiaCompleta;
+	}
+	public void setNoticiaCompleta(Element noticiaCompleta) {
+		this.noticiaCompleta = noticiaCompleta;
+	}
+	public Element getBotonSuscribirse() {
+		return botonSuscribirse;
+	}
+	public void setBotonSuscribirse(Element botonSuscribirse) {
+		this.botonSuscribirse = botonSuscribirse;
+	}
 
 }
