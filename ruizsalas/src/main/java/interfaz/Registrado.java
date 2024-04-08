c6f3c4e0893133a087642d89583a47c6c7fd7892
Daniel Salas <dsa069@inlumine.ual.es>
2024-04-08 16:25:23 +0200

    pum arreglao

--- ruizsalas/src/main/java/interfaz/Registrado.java
+++ ruizsalas/src/main/java/interfaz/Registrado.java
@@ -4,9 +4,7 @@ package interfaz;
 public class Registrado extends Usuario {
 	public Registrado (proyectoMDS.MainView mainView){
 		super(mainView);
-		
-		this.getZonaAnuncios().setVisible(false);
-		this.getZonaAnuncios2().setVisible(false);
+	
 	}
 //	public iRegistrado _iRegistrado;
 //	public Editar_Perfil _accede;
