abadcc2fb35709500e9f26f235eacc7c8a09cc91
crv520-1 <crv520@inlumine.ual.es>
2024-04-25 10:22:49 +0200

    Stephen Curry desde detrás de medio campo tira yyyyyyyyy... (Continuará)

--- ruizsalas/src/main/java/vistas/VistaSeleccionartematica_item.java
+++ ruizsalas/src/main/java/vistas/VistaSeleccionartematica_item.java
@@ -3,13 +3,26 @@ package vistas;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.template.Id;
 
 @Tag("vista-seleccionartematica_item")
 @JsModule("./src/vista-seleccionartematica_item.ts")
 public class VistaSeleccionartematica_item extends LitTemplate {
 
-    public VistaSeleccionartematica_item() {
+    @Id("tematicaAAñadir")
+	private Checkbox tematicaAAñadir;
+
+	public VistaSeleccionartematica_item() {
         // You can initialise any data required for the connected UI components here.
     }
+
+	public Checkbox getTematicaAAñadir() {
+		return tematicaAAñadir;
+	}
+
+	public void setTematicaAAñadir(Checkbox tematicaAAñadir) {
+		this.tematicaAAñadir = tematicaAAñadir;
+	}
 
 }
