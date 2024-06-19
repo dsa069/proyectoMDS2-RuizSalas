c964a24fff129a67bec4ef2e5a41b95b86c89497
crv520-1 <crv520@inlumine.ual.es>
2024-06-19 11:09:59 +0200

    Simple y llanamente position absolute

--- ruizsalas/frontend/src/vista-mensajedeerror.ts
+++ ruizsalas/frontend/src/vista-mensajedeerror.ts
@@ -14,8 +14,8 @@ export class VistaMensajedeerror extends LitElement {
 
   render() {
     return html`
-<vaadin-horizontal-layout class="content" style="width: 100%; height: 100%; position: absolute; justify-content: center; align-items: center; font-size: 100px">
- <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-penrKPhqk6m6ya-citqeT12YQjkAmALUZgCVTwIgr83v-Cpp" width="30%" height="50%" style="align-self: center;">NO SE HAN ENCONTRADO RESULTADOS PARA LA BÚSQUEDA
+<vaadin-horizontal-layout class="content" style="width: 100%; height: 100%; justify-content: center; align-items: center; font-size: 100px">
+ <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-penrKPhqk6m6ya-citqeT12YQjkAmALUZgCVTwIgr83v-Cpp" width="30%" height="50%" style="align-self: center;">NO SE HAN ENCONTRADO RESULTADOS PARA LA BÚSQUEDA 
 </vaadin-horizontal-layout>
 `;
   }
