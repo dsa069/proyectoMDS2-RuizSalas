e68a56764f9a790c9c06376ab8b51bfc85f9fa0c
crv520-1 <crv520@inlumine.ual.es>
2024-06-19 13:43:23 +0200

    anuncios por un tubo

--- ruizsalas/frontend/src/vista-zonaanuncios.ts
+++ ruizsalas/frontend/src/vista-zonaanuncios.ts
@@ -32,6 +32,38 @@ export class VistaZonaanuncios extends LitElement {
  <vaadin-button theme="icon" aria-label="Add new" style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
   <img src="https://pbs.twimg.com/media/F8UzfyjWIAEB0E6.jpg" style="width: 90%; height: 75%; display: block;">
  </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://imagenes.heraldo.es/files/image_990_556/uploads/imagenes/2020/02/10/colacao.jpeg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://www.nesquik.es/sites/default/files/2023-03/Banner-NQ-768x768_0.jpg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://i.pinimg.com/originals/2b/76/6b/2b766b89e70a779f895b2403dc26027f.jpg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEil_sCiYwZWJzieGToeyNis99iZRaNJjkVHTkvQ_IG50i_ytnvjYk2ZDapB2X5L1Wjw-bm0LHM-eGJ0fnu4gonwbrwjB89Wz2mPTgyiPQifsix5SHuX1fyz9XPqaaCQJ237NKss9KD9Qj8/s1600/coca+cola+mejor+abiertos.jpg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://www.ahorramas.com/on/demandware.static/-/Sites-ahorramas-master/default/dwca81a5d1/Assets/043986_Z1NL/large/2/7/1/7/2717fd8fd02a5267a86ceb925579a3cb0d98fc09_043986_Z1NL.jpg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://miro.medium.com/v2/resize:fit:819/0*ERlWUfkT5WZ04AlT.jpg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://static.mercadonegro.pe/wp-content/uploads/2023/05/29134632/APOYO-4.jpg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
+ <hr style="flex-shrink: 0; width: 90%;">
+ <vaadin-button style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
+  <img src="https://epinium.com/wp-content/uploads/2018/09/amazon-advertising.jpg" style="width: 90%; height: 75%; display: block;">
+ </vaadin-button>
 </vaadin-vertical-layout>
 `;
 	}
