0e4363773c3bf80cf071898596a70049ba86df34
crv520-1 <crv520@inlumine.ual.es>
2024-06-19 13:26:23 +0200

    escudo betis

--- ruizsalas/frontend/src/vista-zonaanuncios.ts
+++ ruizsalas/frontend/src/vista-zonaanuncios.ts
@@ -18,15 +18,15 @@ export class VistaZonaanuncios extends LitElement {
 		return html`
 <vaadin-vertical-layout style="flex-shrink: 0; width: 100%; height: 100%;">
  <vaadin-button theme="icon" aria-label="Add new" style="width: 100%; flex-shrink: 0; flex-grow: 0; overflow: hidden; height: 35%; background-color: var(--lumo-contrast-0pct);">
-  <img src="https://i.pinimg.com/originals/46/f3/18/46f318120f97b7f472ecbb43ac2ef20e.jpg" style="width: 90%; height: 75%; display: block; margin: var(--lumo-space-s);" id="imagenZonaDeAnuncios">
+  <img src="https://marcadegol.com/fotos//2014/06/Messi-Lays-Brasil-2014.jpg" style="width: 90%; height: 75%; display: block; margin: var(--lumo-space-s);" id="imagenZonaDeAnuncios">
  </vaadin-button>
  <hr style="width: 90%; flex-shrink: 0;">
  <vaadin-button theme="icon" aria-label="Add new" style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 20%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
-  <img src="https://i.redd.it/el59y0snhf0a1.jpg" style="width: 90%; height: 75%; display: block; margin: var(--lumo-space-s);" id="imagenZonaDeAnuncios1">
+  <img src="https://cdn.bitlysdowssl-aws.com/wp-content/uploads/2021/10/logotipo-Eurocopa-Alemania-Foto-Archivo.jpg" style="width: 90%; height: 75%; display: block; margin: var(--lumo-space-s);" id="imagenZonaDeAnuncios1">
  </vaadin-button>
  <hr style="width: 90%; flex-shrink: 0;">
  <vaadin-button theme="icon" aria-label="Add new" style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 17%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
-  <img src="https://pbs.twimg.com/media/EyzRrIPWgAgnRSS.jpg" style="width: 90%; height: 75%; display: block; margin: var(--lumo-space-s);">
+  <img src="https://cdn.resfu.com/media/img_news/leo-messi--en-un-comercial-de-adidas.jpg?size=1000x&amp;lossy=1" style="width: 90%; height: 75%; display: block;">
  </vaadin-button>
  <hr style="flex-shrink: 0; width: 90%;">
  <vaadin-button theme="icon" aria-label="Add new" style="width: 100%; flex-shrink: 0; flex-grow: 0; height: 30%; overflow: hidden; background-color: var(--lumo-contrast-0pct);">
