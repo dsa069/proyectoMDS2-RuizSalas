b8017c42da76c4a845f4e4c9f586d31914d97f8b
Daniel Salas <dsa069@inlumine.ual.es>
2024-06-20 09:14:52 +0200

    porque me grita

--- ruizsalas/src/main/java/interfaz/Lista_secciones_Editor_item.java
+++ ruizsalas/src/main/java/interfaz/Lista_secciones_Editor_item.java
@@ -1,6 +1,5 @@
 package interfaz;
 
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import basededatos.BD_Principal;
@@ -26,7 +25,6 @@ public class Lista_secciones_Editor_item extends Listar_Secciones_item {
 	
 	@Override
 	public void Listar_Noticia() {
-		Notification.show("AAAAAAAAAAAAAAA");
 		this._contiene = new Lista_noticas_Editor(this._editor, this);
 		this.getListaDeNoticiasEstatico().add(this._contiene);
 		this._contiene.Noticia_item(seccion.getIdSeccion());
