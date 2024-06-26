32f82e7913d1c3e018a90375f1dea57393759daf
Daniel Salas <dsa069@inlumine.ual.es>
2024-06-26 18:19:43 +0200

    jeje

--- ruizsalas/src/main/java/interfaz/Listar_noticias_item.java
+++ ruizsalas/src/main/java/interfaz/Listar_noticias_item.java
@@ -88,7 +88,6 @@ public class Listar_noticias_item extends Listar_noticias_generico_item {
 				}
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
